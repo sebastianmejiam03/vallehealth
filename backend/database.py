9f6afc5b4949b6d3da72de4330a402da417b4ffb
Sebastian Mejia <sebastianmejia@MacBook-Pro-de-Sebastian.local>
2026-03-16 22:16:10 -0500

    Configuración base de datos Render y requirements

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://sebastianmejia@localhost/vallehealth"
+DATABASE_URL = "postgresql://vallehealth_user:s1TkCtRlEeM8IkG6Zuv6O3qUIZnlcpzJ@dpg-d6sc99pj16oc73em07h0-a.oregon-postgres.render.com/vallehealth"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -14,4 +14,3 @@ def get_db():
         yield db
     finally:
         db.close()
-        
